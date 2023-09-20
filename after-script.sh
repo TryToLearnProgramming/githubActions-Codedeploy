3918aa400abe5e77ca757ba3441adeefdbcf074f
devops-sami <sami@codelogicx.com>
2023-09-20 12:19:45 +0530

    test27

--- after-script.sh
+++ after-script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd /home/ubuntu/new-app
+
 docker build -t test .
 docker run --name cont -d -p 80:80 test
--- before-script.sh
+++ before-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -aq) > /dev/null 2>&1
-docker rmi -f $(docker images -f "dangling=true" -q) > /dev/null 2>&1
-docker rmi -f $(docker images test -q) > /dev/null 2>&1
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -f "dangling=true" -q)
+docker rmi -f $(docker images test -q)
